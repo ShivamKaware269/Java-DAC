ed76eae0439cc07424dda80a19efb982fd5325d6
Shivam Kaware <shivamkaware269@gmail.com>
2026-07-12 19:50:27 +0530

    Add files via upload

---
+++ Fundamentals/Basic Java Programs/Circle_Area.java
@@ -0,0 +1,28 @@
+// Area Of Circle Java Program
+
+import java.util.Scanner;
+
+public class Circle_Area {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Select the dimensions \n1.Radius \n2.Diameter");
+        System.out.print("Choice : ");
+        int choice = sc.nextInt();
+        float pi = 3.14f;
+
+        if (choice == 1) {
+            System.out.print("Enter the radius of the Circle : ");
+            int rad = sc.nextInt();
+            System.out.print("The area of circle is : " + (pi*rad*rad)); //Type casting
+        } else if (choice == 2) {
+            System.out.print("Enter the diameter of the Circle : ");
+            int dia = sc.nextInt();
+            System.out.print("The area of circle is : " + (pi*(dia/2)*(dia/2))); //Type casting
+        } else {
+            System.out.print("Invalid choice");
+        }
+        
+        sc.close();
+    }
+    
+}
