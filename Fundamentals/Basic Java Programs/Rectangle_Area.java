80b9aeb718075d73668b5472f21d2ac9a511247a
Shivam Kaware <shivamkaware269@gmail.com>
2026-07-12 19:55:38 +0530

    Add Rectangle Area calculation program

--- Fundamentals/Basic Java Programs/Rectangle_Area.java
+++ Fundamentals/Basic Java Programs/Rectangle_Area.java
@@ -1,1 +1,20 @@
+// Area Of Rectangle Program
+
+import java.util.Scanner;
+
+public class Rectangle_Area {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the length of the rectangle : ");
+        int l = sc.nextInt();
+        System.out.print("Enter the bredth of the rectangle : ");
+        float b = sc.nextFloat();
+
+        System.out.println("The Area of the rectangle : " + (b*l)); //Type casting
+
+        sc.close();
+    }
+}
+
 
