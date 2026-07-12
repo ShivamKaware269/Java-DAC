757c87139c261e6ca9f8d907e57dd1b6797e727b
Shivam Kaware <shivamkaware269@gmail.com>
2026-07-12 19:53:35 +0530

    Add program to calculate area of a parallelogram

--- Fundamentals/Basic Java Programs/Parallelogram_Area.java
+++ Fundamentals/Basic Java Programs/Parallelogram_Area.java
@@ -1,1 +1,18 @@
+// Area Of Parallelogram
 
+import java.util.Scanner;
+
+public class Parallelogram_Area {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the base of the parallelogram : ");
+        float b = sc.nextFloat();
+        System.out.print("Enter the perpendicular height of the parallelogram : ");
+        float h = sc.nextFloat();
+
+        System.out.println("The Area of the parallelogram : " + (b * h));
+
+        sc.close();
+    }
+    
+}
