0e6bbb4f3fc8e2a1daf1d2956ce4ddaf59c0d4bb
Shivam Kaware <shivamkaware269@gmail.com>
2026-07-12 19:52:38 +0530

    Add program to calculate area of equilateral triangle

--- Fundamentals/Basic Java Programs/Equilateral_Triangle_Area.java
+++ Fundamentals/Basic Java Programs/Equilateral_Triangle_Area.java
@@ -1,1 +1,16 @@
+// Area Of Equilateral Triangle
+
+import java.util.Scanner;
+
+public class Equilateral_Triangle_Area {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the length of the side of the equilateral triangle : ");
+        float a = sc.nextFloat();
+
+        System.out.println("The Area of the equilateral triangle : " + ((Math.sqrt(3)/4)*a*a));
+
+        sc.close();
+    }
+}
 
