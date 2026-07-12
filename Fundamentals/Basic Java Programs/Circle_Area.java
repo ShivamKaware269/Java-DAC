0a3b3a2dc50c869af45f0c63137668ef4b85a2ef
Shivam Kaware <shivamkaware269@gmail.com>
2026-07-12 20:11:13 +0530

    Enhance Circle Area program output format
    Added unit 'sq.m' to area output for clarity.

--- Fundamentals/Basic Java Programs/Circle_Area.java
+++ Fundamentals/Basic Java Programs/Circle_Area.java
@@ -13,11 +13,11 @@ public class Circle_Area {
         if (choice == 1) {
             System.out.print("Enter the radius of the Circle : ");
             int rad = sc.nextInt();
-            System.out.print("The area of circle is : " + (pi*rad*rad)); //Type casting
+            System.out.print("The area of circle is : " + (pi*rad*rad) + " sq.m"); //Type casting
         } else if (choice == 2) {
             System.out.print("Enter the diameter of the Circle : ");
             int dia = sc.nextInt();
-            System.out.print("The area of circle is : " + (pi*(dia/2)*(dia/2))); //Type casting
+            System.out.print("The area of circle is : " + (pi*(dia/2)*(dia/2)) + " sq.m"); //Type casting
         } else {
             System.out.print("Invalid choice");
         }
