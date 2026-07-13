ce6c53a2f32609782fce4d56c6551144524a3e02
Shivam Kaware <shivamkaware269@gmail.com>
2026-07-13 19:29:56 +0530

    Add program to calculate the area of a triangle

--- Fundamentals/Basic Java Programs/Triangle_Area.java
+++ Fundamentals/Basic Java Programs/Triangle_Area.java
@@ -1,1 +1,18 @@
+// Area Of Triangle
+
+import java.util.Scanner;
+
+public class Triangle_Area {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the height of the triangle : ");
+        int h = sc.nextInt();
+        System.out.print("Enter the base of the triangle : ");
+        float b = sc.nextFloat();
+
+        System.out.println("The Area of the triangle : " + (0.5*b*h) + " sqm"); //Type casting
+
+        sc.close();
+    }
+}
 
