7f7ad0ac5971457702f904ee63e2bac5f84ec5e2
Shivam Kaware <shivamkaware269@gmail.com>
2026-07-14 20:42:58 +0530

    Calculate volume of cone based on user input
    Fix volume calculation by using correct formula.

--- Fundamentals/Basic Java Programs/Cone_Volume.java
+++ Fundamentals/Basic Java Programs/Cone_Volume.java
@@ -1,1 +1,22 @@
+// Volume of cone
+
+import java.util.Scanner;
+
+public class Cone_Volume {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the radius of the cone : ");
+        float r = sc.nextFloat();
+        System.out.print("Enter the height of the cone : ");
+        float h = sc.nextFloat();
+
+        float pi = 3.14f;
+
+        float volume = (1/3)*pi*r*r*h;
+
+        System.out.println("The volume of the cone : " + volume + " cu.unit");
+
+        sc.close();
+    }
+}
 
