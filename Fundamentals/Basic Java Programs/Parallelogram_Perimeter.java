d0bb906126b92268ee3ed1851a0b833074948f27
Shivam Kaware <shivamkaware269@gmail.com>
2026-07-14 20:41:45 +0530

    Add Java program to calculate parallelogram perimeter

--- Fundamentals/Basic Java Programs/Parallelogram_Perimeter.java
+++ Fundamentals/Basic Java Programs/Parallelogram_Perimeter.java
@@ -1,1 +1,18 @@
+// Perimeter of a Parallelogram
+
+import java.util.Scanner;
+
+public class Parallelogram_Perimeter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the length of the base of the parallelogram : ");
+        float b = sc.nextFloat();
+        System.out.print("Enter the length of the side of the parallelogram : ");
+        float s = sc.nextFloat();
+
+        System.out.println("The Perimeter of the parallelogram : " + (2 * (b + s)) + " units");
+
+        sc.close();
+    }
+}
 
