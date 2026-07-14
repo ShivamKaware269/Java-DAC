be0bea6879f1ac57c5c605e83cbb60e2021cc856
Shivam Kaware <shivamkaware269@gmail.com>
2026-07-14 20:45:00 +0530

    Update Prism_Volume.java

--- Fundamentals/Basic Java Programs/Prism_Volume.java
+++ Fundamentals/Basic Java Programs/Prism_Volume.java
@@ -1,1 +1,17 @@
+// Volume Of Prism
 
+import java.util.Scanner;
+
+public class Prism_Volume {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the base area of the prism : ");
+        float b = sc.nextFloat();
+        System.out.print("Enter the height of the prism : ");
+        float h = sc.nextFloat();
+
+        System.out.println("The Volume of the prism : " + (b * h) + " cu.units");
+
+        sc.close();
+    }
+}
