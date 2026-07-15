6b0da6a907ca5d9b9fb7aff0568257ede7db0816
Shivam Kaware <shivamkaware269@gmail.com>
2026-07-15 19:34:26 +0530

    Update Sphere_Volume.java

--- 02-Conditionals And Loops/Basic Java Programs/Sphere_Volume.java
+++ 02-Conditionals And Loops/Basic Java Programs/Sphere_Volume.java
@@ -1,1 +1,17 @@
+// Volume Of Sphere
+
+import java.util.Scanner;
+
+public class Sphere_Volume {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the radius of the sphere : ");
+        float r = sc.nextFloat();
+
+        System.out.println("The Volume of the sphere : " + ((4.0 / 3) * Math.PI * Math.pow(r, 3))); 
+        // USE of Math.PI and Math.pow() methods to calculate the volume of the sphere
+
+        sc.close();
+    }
+}
 
