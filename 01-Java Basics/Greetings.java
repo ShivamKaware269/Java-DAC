efe953e23cc2303753fa4e36610959c3f2c5fdd6
Shivam Kaware <shivamkaware269@gmail.com>
2026-07-16 19:38:12 +0530

    Implement greeting message based on user input
    Added functionality to take user input for a name and print a personalized greeting message.

--- 01-Java Basics/Greetings.java
+++ 01-Java Basics/Greetings.java
@@ -1,1 +1,17 @@
+// Take name as input and print a greeting message for that particular name.
+
+import java.util.Scanner;
+
+public class Greeting {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the name");
+
+        String name = sc.next();
+
+        System.out.println("Hello " + name + " !! Great to see you here");
+        
+        sc.close();
+    }
+}
 
