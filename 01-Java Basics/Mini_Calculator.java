c115915f94d1718dd58eede431384039f487c010
Shivam Kaware <shivamkaware269@gmail.com>
2026-07-17 19:43:51 +0530

    Correct operator handling in Mini_Calculator
    Fix calculation and output for multiplication and division.

--- 01-Java Basics/Mini_Calculator.java
+++ 01-Java Basics/Mini_Calculator.java
@@ -1,1 +1,31 @@
+// Take in two numbers and an operator (+, -, *, /) and calculate the value. (Use if conditions)
+
+import java.util.Scanner;
+
+public class Mini_Calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the first number : ");
+        int a = sc.nextInt();
+        System.out.print("Enter the operator (+,-,*,/) :");
+        char o = sc.next().charAt(0);
+        System.out.print("Enter the Second number : ");
+        int b = sc.nextInt();
+
+        if (o == '+') {
+            System.out.print("Addition : "+ (a+b));
+        } else if (o == '-') {
+            System.out.print("Subtraction : "+ (a-b));
+        } else if (o == '*') {
+            System.out.print("Subtraction : "+ (a*b));    
+        } else if (o == '-') {
+            System.out.print("Subtraction : "+ (a/b));    
+        } else {
+            System.out.println("Invalid operation");
+        }
+
+        sc.close();
+    }
+}
 
