952ae977f296447b7d1deb239d25140e74cf7575
Shivam Kaware <shivamkaware269@gmail.com>
2026-07-17 19:42:24 +0530

    Implement program to find the largest of two numbers

--- 01-Java Basics/Largest_Of_Two.java
+++ 01-Java Basics/Largest_Of_Two.java
@@ -1,1 +1,23 @@
+// Take 2 numbers as input and print the largest number.
+
+import java.util.Scanner;
+
+public class Largest_Of_Two {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the first number : ");
+        int a = sc.nextInt();
+        System.out.print("Enter the second number : ");
+        int b = sc.nextInt();
+
+        if (a>b) {
+            System.out.println("Largest : " + a);  
+        } else {
+            System.out.println("Largest : " + b);       
+        }
+
+        sc.close();
+    }
+}
 
