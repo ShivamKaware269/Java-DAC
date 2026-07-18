c354f8b195e503b27f650f28e8a9dc0b038ecdf0
Shivam Kaware <shivamkaware269@gmail.com>
2026-07-18 14:14:48 +0530

    Implement INR to USD conversion program
    Added a Java program to convert INR to USD.

--- 01-Java Basics/INR_To_USD.java
+++ 01-Java Basics/INR_To_USD.java
@@ -1,1 +1,17 @@
+// Input currency in rupees and output in USD.
 
+import java.util.Scanner;
+
+public class INR_To_USD {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    
+        System.out.print("Enter the value in rupees : ");
+        int inr = sc.nextInt();
+        double usd = 0.011; //As of on 18-July-2026
+
+        System.out.println("The value in dollar is " +  (inr * usd));
+
+        sc.close();
+    }    
+}
