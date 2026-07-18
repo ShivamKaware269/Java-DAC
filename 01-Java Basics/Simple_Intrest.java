872b1257f5f63fbd80a63d0599cd032e034ff238
Shivam Kaware <shivamkaware269@gmail.com>
2026-07-18 14:16:10 +0530

    Implement Simple Interest calculation program

--- 01-Java Basics/Simple_Intrest.java
+++ 01-Java Basics/Simple_Intrest.java
@@ -1,1 +1,24 @@
+// Write a program to input principal, time, and rate (P, T, R) from the user and find Simple Interest.
+
+import java.util.Scanner;
+
+public class Simple_Intrest {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the Principal amount :");
+        int P = sc.nextInt();
+        System.out.print("Enter the time period :");
+        float T = sc.nextFloat();
+        System.out.print("Enter the rate of intrest :");    
+        float R = sc.nextFloat();
+
+        float SI = (P*T*R)/100;
+
+        System.out.println("The simple intrest would be : " + SI);
+
+        sc.close();
+    }
+}
 
