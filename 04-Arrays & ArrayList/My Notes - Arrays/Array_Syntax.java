6f1574ed39b0d500a36c60a71261ceb05bf2a9be
Shivam Kaware <shivamkaware269@gmail.com>
2026-07-19 11:56:44 +0530

    Add files via upload

---
+++ 04-Arrays & ArrayList/My Notes - Arrays/Array_Syntax.java
@@ -0,0 +1,19 @@
+public class Array_Syntax {
+    public static void main(String[] args) {
+
+        // 1.Initilization
+        int[] Array1;
+
+        // 2.Initilization and Declaration with size
+        int[] Array2 = new int[10]; // datatype[] ArrayVariableName = new datatype[SizeOfArray];
+
+
+        // 3.Initilization and Declaration with size seperately
+        int[] Array3;
+        Array3 = new int[8];
+
+        // 4.Declaration and Assignment of values
+        int[] Array4 = { 1, 2, 6, 8, 10, 15};
+        
+    }
+}
