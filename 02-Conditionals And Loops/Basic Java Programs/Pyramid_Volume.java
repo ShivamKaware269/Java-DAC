7e31ddd100e69be83541e29b33c7cc838feac8ff
Shivam Kaware <shivamkaware269@gmail.com>
2026-07-20 21:03:31 +0530

    Implement pyramid volume calculation in Java

--- 02-Conditionals And Loops/Basic Java Programs/Pyramid_Volume.java
+++ 02-Conditionals And Loops/Basic Java Programs/Pyramid_Volume.java
@@ -1,1 +1,18 @@
+// Volume of a pyramid = (1/3) * base area * height
+
+import java.util.Scanner;
+
+public class Pyramid_Volume {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the base area of the pyramid : ");
+        float b = sc.nextFloat();
+        System.out.print("Enter the height of the pyramid : ");
+        float h = sc.nextFloat();
+
+        System.out.println("The Volume of the pyramid : " + ((1.0 / 3) * b * h));
+
+        sc.close();
+    }
+}
 
