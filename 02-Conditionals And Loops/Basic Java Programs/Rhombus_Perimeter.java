3f15c59885093286e892705a26e78ea6b15af368
Shivam Kaware <shivamkaware269@gmail.com>
2026-07-21 21:16:25 +0530

    Add Java program to calculate rhombus perimeter

--- 02-Conditionals And Loops/Basic Java Programs/Rhombus_Perimeter.java
+++ 02-Conditionals And Loops/Basic Java Programs/Rhombus_Perimeter.java
@@ -1,1 +1,16 @@
+// Perimeter of Rhombus
+
+import java.util.Scanner;   
+
+public class Rhombus_Perimeter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the length of the side of the rhombus : ");
+        float s = sc.nextFloat();
+
+        System.out.println("The Perimeter of the rhombus : " + (4 * s));
+
+        sc.close();
+    }
+}
 
