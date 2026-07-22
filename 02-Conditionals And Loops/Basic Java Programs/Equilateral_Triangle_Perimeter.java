e792623a2e9826fc920b2c2c8f4d143f0d11356e
Shivam Kaware <shivamkaware269@gmail.com>
2026-07-22 19:24:15 +0530

    Add program to calculate equilateral triangle perimeter

--- 02-Conditionals And Loops/Basic Java Programs/Equilateral_Triangle_Perimeter.java
+++ 02-Conditionals And Loops/Basic Java Programs/Equilateral_Triangle_Perimeter.java
@@ -1,1 +1,16 @@
+// Perimeter of an Equilateral Triangle
+
+import java.util.Scanner;
+
+public class Equilateral_Triangle_Perimeter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the length of the side of the equilateral triangle : ");
+        float s = sc.nextFloat();
+
+        System.out.println("The Perimeter of the equilateral triangle : " + (3 * s));
+
+        sc.close();
+    }
+}
 
