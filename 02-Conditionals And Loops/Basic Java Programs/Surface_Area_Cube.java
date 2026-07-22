676f7e76868f7952a76e2a656d218de9736beab5
Shivam Kaware <shivamkaware269@gmail.com>
2026-07-22 19:26:28 +0530

    Update Surface_Area_Cube.java

--- 02-Conditionals And Loops/Basic Java Programs/Surface_Area_Cube.java
+++ 02-Conditionals And Loops/Basic Java Programs/Surface_Area_Cube.java
@@ -1,1 +1,16 @@
+// Total surface area of a cube is 6 * side * side
+
+import java.util.Scanner;
+
+public class Surface_Area_Cube {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the length of the side of the cube : ");
+        float s = sc.nextFloat();
+
+        System.out.println("The Total Surface Area of the cube : " + (6 * s * s));
+
+        sc.close();
+    }
+}
 
