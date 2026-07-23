ba159b2ff2ccd53fcbf2a5fb9d288ff538b43a94
Shivam Kaware <shivamkaware269@gmail.com>
2026-07-23 19:55:03 +0530

    Add Fibonacci series program in Java
    Implement Fibonacci series generation with user input.

--- 02-Conditionals And Loops/Basic Java Programs/Fibonacci.java
+++ 02-Conditionals And Loops/Basic Java Programs/Fibonacci.java
@@ -1,1 +1,29 @@
+// Fibonacci Series In Java Programs
+
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of terms for the Fibonacci series : ");
+        int n = sc.nextInt();
+
+        int a = 0, b = 1, c;
+        System.out.print("The Fibonacci series is : ");
+
+        for (int i = 1; i <= n; i++) {
+            if (i == 1) {
+                System.out.print(a + " ");
+            } else if (i == 2) {
+                System.out.print(b + " ");
+            } else {
+                c = a + b;
+                System.out.print(c + " ");
+                a = b;
+                b = c;
+            }
+        }
+        sc.close();
+    }
+}
 
