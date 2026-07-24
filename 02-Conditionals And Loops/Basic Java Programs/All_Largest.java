a7e8994c32c650feeff58a304aedc351b7c37d37
Shivam Kaware <shivamkaware269@gmail.com>
2026-07-24 21:28:38 +0530

    Add program to find the largest number from inputs
    Implemented a program to find the largest number from user inputs until 0 is entered.

--- 02-Conditionals And Loops/Basic Java Programs/All_Largest.java
+++ 02-Conditionals And Loops/Basic Java Programs/All_Largest.java
@@ -1,1 +1,33 @@
+// Take integer inputs till the user enters 0 and print the largest number from all.
+
+import java.util.Scanner;
+
+public class All_Largest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = 1, largest = 0;
+
+        while (t == 1) {
+
+            System.out.print("Enter the number and to stop Enter '0' : ");
+            int x = sc.nextInt();
+            
+
+            if (x == 0) {
+                break;
+            } else {
+                if (x > largest) {
+                    largest = x;
+                }
+            }
+
+
+        }
+        System.out.println("The largest of all the numbers is : " + largest);
+
+        sc.close();
+    }
+}
+
 
