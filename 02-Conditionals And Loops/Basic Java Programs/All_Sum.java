de50ce49bbfed0581648d25da346ebf0674de837
Shivam Kaware <shivamkaware269@gmail.com>
2026-07-24 21:27:16 +0530

    Add All_Sum program to calculate sum of integers
    Implement a program to sum user-input integers until 0 is entered.

--- 02-Conditionals And Loops/Basic Java Programs/All_Sum.java
+++ 02-Conditionals And Loops/Basic Java Programs/All_Sum.java
@@ -1,1 +1,26 @@
+// Take integer inputs till the user enters 0 and print the sum of all numbers
+
+import java.util.Scanner;
+
+public class All_Sum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = 1, sum = 0;
+
+        while (t == 1) {
+
+            System.out.print("Enter the number and to stop Enter '0' : ");
+            int x = sc.nextInt();
+            
+            if (x == 0) {
+                break;
+            } else {
+                sum = sum + x;
+            }
+
+        }
+        System.out.println("The sum of all the numbers is : " + sum);
+        sc.close();
+    }
+}
 
