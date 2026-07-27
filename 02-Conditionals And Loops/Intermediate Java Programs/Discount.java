6342dbf6c83e05aa9b95787093ab3b4733b860f0
Shivam Kaware <shivamkaware269@gmail.com>
2026-07-27 21:31:36 +0530

    Implement discount calculation in Discount.java

--- 02-Conditionals And Loops/Intermediate Java Programs/Discount.java
+++ 02-Conditionals And Loops/Intermediate Java Programs/Discount.java
@@ -1,1 +1,24 @@
+// Calculate Discount Of Product
+
+import java.util.Scanner;
+
+public class Discount {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the original price of the product : ");
+        double originalPrice = sc.nextDouble();
+
+        System.out.print("Enter the discount percentage : ");
+        double discountPercentage = sc.nextDouble();
+
+        double discountAmount = (discountPercentage / 100) * originalPrice;
+        double finalPrice = originalPrice - discountAmount;
+
+        System.out.println("Discount Amount : " + discountAmount);
+        System.out.println("Final Price after Discount : " + finalPrice);
+
+        sc.close();
+    }
+}
 
