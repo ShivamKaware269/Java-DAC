2a7dae0b259a57a64543e9fa57cdaf90c7b64411
Shivam Kaware <shivamkaware269@gmail.com>
2026-07-28 20:46:43 +0530

    Implement array building from permutation input

--- 04-Arrays & ArrayList/Easy Problems/Bulid_Array_From_Permutation.java
+++ 04-Arrays & ArrayList/Easy Problems/Bulid_Array_From_Permutation.java
@@ -1,1 +1,25 @@
+import java.util.Arrays;
+import java.util.Scanner;
 
+public class Build_Array_From_Permutation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[6];
+        System.out.println("Enter array elements : ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Initial : " + Arrays.toString(arr));
+
+        int[] ans = new int[6];
+        for (int i = 0; i < arr.length; i++) {
+            ans[i] = arr[arr[i]];
+        }
+
+        System.out.println("Final : " + Arrays.toString(ans));
+
+        sc.close();
+    }
+}
