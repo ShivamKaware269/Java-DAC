45547dadba37d33e317bf417ebc05c830aec2cdd
Shivam Kaware <shivamkaware269@gmail.com>
2026-07-28 20:49:52 +0530

    Add Commission Percentage calculation program

--- 02-Conditionals And Loops/Intermediate Java Programs/Commission_Percentage.java
+++ 02-Conditionals And Loops/Intermediate Java Programs/Commission_Percentage.java
@@ -1,1 +1,22 @@
+// Calculate Commission Percentage
+
+import java.util.Scanner;
+
+public class Commission_Percentage {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the total sales amount : ");
+        double totalSales = sc.nextDouble();
+
+        System.out.print("Enter the commission percentage : ");
+        double commissionPercentage = sc.nextDouble();
+
+        double commissionAmount = (commissionPercentage / 100) * totalSales;
+
+        System.out.println("Commission Amount : " + commissionAmount);
+
+        sc.close();
+    }
+}
 
