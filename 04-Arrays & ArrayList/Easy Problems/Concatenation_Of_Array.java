55d5b9435d2a26253d55326801e18b5048aa7876
Shivam Kaware <shivamkaware269@gmail.com>
2026-07-28 20:36:19 +0530

    Add array concatenation functionality
    This program concatenates an array without using built-in functions by creating a new array and copying elements.

---
+++ 04-Arrays & ArrayList/Easy Problems/Concatenation_Of_Array.java
@@ -0,0 +1,31 @@
+// Perform Concatenation of the Array without using any built-in functions
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Concatenation_Of_Array {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int s = arr.length;
+        int[] conc = new int[2 * s];
+        
+        for (int i = 0; i < s; i++) {
+            conc[i] = arr[i];
+            conc[i + s] = arr[i];
+        }
+
+        System.out.println("Concatenated Array:" + Arrays.toString(conc));
+
+        sc.close();
+    }
+    
+}
