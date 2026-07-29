5ff8d2a659231ba4f6fa6d8e19f9fc22bec75de7
Shivam Kaware <shivamkaware269@gmail.com>
2026-07-29 22:33:13 +0530

    Implement running sum calculation for an array
    Calculate the running sum of an array and print the result.

--- 04-Arrays & ArrayList/Easy Problems/Running_Sum.java
+++ 04-Arrays & ArrayList/Easy Problems/Running_Sum.java
@@ -1,1 +1,17 @@
+import java.util.Arrays;
+
+public class Running_Sum {
+    public static void main(String[] args) {
+        int[] a = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        System.out.println(Arrays.toString(a));
+        int sum = 0;
+
+        for (int i = 1; i < a.length; i++) {
+            sum = a[i] + a[i-1];
+            a[i] = sum;
+        }
+
+        System.out.println(Arrays.toString(a));
+    }
+}
 
