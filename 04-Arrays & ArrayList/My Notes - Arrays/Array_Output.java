84bfc4d13a615a52f62409cf517404118eb5db52
Shivam Kaware <shivamkaware269@gmail.com>
2026-07-29 22:34:19 +0530

    Update Array_Output.java

--- 04-Arrays & ArrayList/My Notes - Arrays/Array_Output.java
+++ 04-Arrays & ArrayList/My Notes - Arrays/Array_Output.java
@@ -1,1 +1,30 @@
+import java.util.Arrays;
+
+public class Array_Output {
+    public static void main(String[] args) {
+        
+        int[] Array = { 9, 5, 2, 6, 3, 7};
+
+        // 1.Simple for loop output
+        for (int i = 0; i < Array.length; i++) {
+            System.out.print( Array[i] + " ");
+        }
+
+        System.out.println();
+
+        // 2.For each loop (iterating each element in the array through "i")
+        for (int i : Array) {
+            System.out.print(i + " ");
+        }
+
+        System.out.println();
+
+        // 3.Using the Array class (Most preferable)
+        System.out.println(Arrays.toString(Array)); // Converts the array into string and prints in array format
+
+        // We can change the value of a particular index as : Array[2] = 35;
+        // Arrays are mutable
+
+    }
+}
 
