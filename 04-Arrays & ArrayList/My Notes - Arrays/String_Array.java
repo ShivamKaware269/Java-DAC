beb3390c351887a85f275c66374a446d35224187
Shivam Kaware <shivamkaware269@gmail.com>
2026-07-30 21:54:19 +0530

    Update String_Array.java

--- 04-Arrays & ArrayList/My Notes - Arrays/String_Array.java
+++ 04-Arrays & ArrayList/My Notes - Arrays/String_Array.java
@@ -1,1 +1,21 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class String_Array {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+       
+        String[] name = new String[5];
+
+        for (int i = 0; i < name.length; i++) {
+            System.out.print("Enter the name at position " + (i+1) + " : ");
+            name[i] = sc.next();
+        }
+
+        System.out.println("The output is : "+ Arrays.toString(name) );
+
+        sc.close();
+    }
+}
 
