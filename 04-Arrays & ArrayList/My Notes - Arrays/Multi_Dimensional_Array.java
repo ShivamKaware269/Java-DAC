f81cc287df2e2f057c98d17ee32150d14b887fcd
Shivam Kaware <shivamkaware269@gmail.com>
2026-07-30 21:56:15 +0530

    Implement 2D array input and output functionality
    This program initializes a 2D array, takes user input to populate it, and then prints the array.

--- 04-Arrays & ArrayList/My Notes - Arrays/Multi_Dimensional_Array.java
+++ 04-Arrays & ArrayList/My Notes - Arrays/Multi_Dimensional_Array.java
@@ -1,1 +1,37 @@
+import java.util.Scanner;
+
+public class Mutli_Dimensional_Array {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        int[][] TwoDimensional = new int[3][3];
+
+        // Input
+        for (int i = 0; i < TwoDimensional.length; i++) {
+
+            for (int j = 0; j < TwoDimensional.length; j++) {
+                System.out.print("Enter the element at position " + (i+1) + " " + (j+1) + " : ");
+                TwoDimensional[i][j] = sc.nextInt();
+            }
+
+        }
+
+        System.out.println("The output is : ");
+
+        // Output
+        for (int i = 0; i < TwoDimensional.length; i++) {
+
+            for (int j = 0; j < TwoDimensional.length; j++) {
+                System.out.print(TwoDimensional[i][j] + " ");
+            }
+
+            System.out.println();
+
+        }
+
+        sc.close();
+
+    }
+}
 
