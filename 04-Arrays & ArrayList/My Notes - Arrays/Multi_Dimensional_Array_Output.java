1e1ef72f4693575ca90bd543ff154903de03d6d6
Shivam Kaware <shivamkaware269@gmail.com>
2026-07-30 21:56:56 +0530

    Implement multi-dimensional array input and output
    Added a multi-dimensional array example with user input and multiple output methods.

--- 04-Arrays & ArrayList/My Notes - Arrays/Multi_Dimensional_Array_Output.java
+++ 04-Arrays & ArrayList/My Notes - Arrays/Multi_Dimensional_Array_Output.java
@@ -1,1 +1,43 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Multi_Dimensional_Array_Output {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[][] Twod = new int[3][3]; //Array[row][col] -> hHre Array.length gives the no. of rows in array
+
+        // Array[row].length gives the no. of elements in each row (No of elements and not values)
+
+        for (int i = 0; i < 3; i++) {
+
+            for (int j = 0; j < 3; j++) {
+                System.out.print("Enter the element at position " + (i+1) + " " + (j+1) + " : ");
+                Twod[i][j] = sc.nextInt();
+            }
+        }
+
+        // Output Method 1
+        for (int i = 0; i < 3; i++) {
+
+            for (int j = 0; j < 3; j++) {
+                System.out.print(Twod[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        // Output Method 2
+        // for (int i = 0; i < Twod.length; i++) {
+        //     System.out.println(Arrays.toString(Twod[3]));
+        // }
+
+        // Output Method 3
+        for (int[] a : Twod) {
+            System.out.println(Arrays.toString(a));
+        }
+
+
+        sc.close();
+    }
+}
 
