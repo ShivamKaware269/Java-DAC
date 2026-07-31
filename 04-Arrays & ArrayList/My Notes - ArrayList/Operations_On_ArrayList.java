1171d5d8273e67048dd2563f5e3d6fc756c1e443
Shivam Kaware <shivamkaware269@gmail.com>
2026-07-31 20:12:39 +0530

    Update Operations_On_ArrayList.java

--- 04-Arrays & ArrayList/My Notes - ArrayList/Operations_On_ArrayList.java
+++ 04-Arrays & ArrayList/My Notes - ArrayList/Operations_On_ArrayList.java
@@ -1,28 +1,34 @@
 import java.util.ArrayList;
-import java.util.Scanner;
 
-public class Multi_Dimensional_ArrayList {
+public class Operations_On_ArrayList {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        
+        ArrayList<Integer> demo = new ArrayList<>(5);
 
-        ArrayList<ArrayList<Integer>> matrix = new ArrayList<>(); // Declaration of 2D arraylist
+        demo.add(11); // Adding elements to ArrayList
+        demo.add(21);
+        demo.add(31);
+        demo.add(41);
+        demo.add(51);
+        demo.add(61);
+        demo.add(71);
 
-        // We need to first enter few elements in arraylist in order to get some in it
-        for (int i = 0; i < 3; i++) {
-            matrix.add(new ArrayList<>()); // Creating Input space for elements into 2D array
-        }
+        System.out.println(demo);
 
-        // as i = j = 3 its a (3x3) matrix / 2D arraylist
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                System.out.print("Enter the element at " + i + " " + j + " : " ); 
-                matrix.get(i).add(sc.nextInt()); // Inputting the elements
-            }
-        }
+        demo.set(0, 99); // Changing the value of the elssement in Arraylist according to index
 
-        System.out.println(matrix);
+        System.out.println(demo);
 
-        sc.close();
+        System.out.println(demo.contains(99)); // Checks if element is present or not (Boolean Value)
+
+        demo.remove(6); // Removes the element at entered index
+        System.out.println(demo);
+
+        demo.get(5); // To get the valu at the entered index
+
+        /* Working of array list -> When the array list is initilized with a size and with this all the elements 
+        are entered till arraylist is full, here if we add a element after arraylist gets full (internally) a 
+        new arraylist (of doubled size) is generated along with this step the previous arraylist elements gets copied 
+        into the new arraylist with extra size for upcoming elements */
     }
 }
-
