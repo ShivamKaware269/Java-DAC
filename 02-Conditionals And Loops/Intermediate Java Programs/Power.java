3054923474528e342e247e825a6c4d2025545f39
Shivam Kaware <shivamkaware269@gmail.com>
2026-08-01 18:08:57 +0530

    Implement power calculation using Math.pow()

--- 02-Conditionals And Loops/Intermediate Java Programs/Power.java
+++ 02-Conditionals And Loops/Intermediate Java Programs/Power.java
@@ -1,1 +1,22 @@
+// Power In Java
+
+import java.util.Scanner;
+
+public class Power {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the base number : ");
+        double base = sc.nextDouble();
+
+        System.out.print("Enter the exponent : ");
+        double exponent = sc.nextDouble();
+
+        double result = Math.pow(base, exponent); // Power calculation using Math.pow() method
+
+        System.out.println(base + " raised to the power of " + exponent + " is : " + result);
+
+        sc.close();
+    }
+}
 
