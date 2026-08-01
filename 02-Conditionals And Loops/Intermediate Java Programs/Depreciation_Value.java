da3cddc0c5ae6a81cb00a015ed5b586599267a9b
Shivam Kaware <shivamkaware269@gmail.com>
2026-08-01 18:09:57 +0530

    Implement depreciation calculation in Depreciation_Value

--- 02-Conditionals And Loops/Intermediate Java Programs/Depreciation_Value.java
+++ 02-Conditionals And Loops/Intermediate Java Programs/Depreciation_Value.java
@@ -1,1 +1,24 @@
+// Calculate Depreciation of Value
+
+import java.util.Scanner;
+
+public class Depreciation_Value {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the original value of the asset : ");
+        double originalValue = sc.nextDouble();
+
+        System.out.print("Enter the depreciation percentage at current time : ");
+        double depreciationPercentage = sc.nextDouble();
+
+        double depreciationAmount = (depreciationPercentage / 100) * originalValue;
+        double finalValue = originalValue - depreciationAmount;
+
+        System.out.println("Depreciation Amount : " + depreciationAmount);
+        System.out.println("Final Value after Depreciation : " + finalValue);
+
+        sc.close();
+    }
+}
 
