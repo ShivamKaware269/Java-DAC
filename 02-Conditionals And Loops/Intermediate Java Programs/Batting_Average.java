13848410568fa689b6ea95d2edf252d8d5ff6a89
Shivam Kaware <shivamkaware269@gmail.com>
2026-08-02 21:20:06 +0530

    Implement batting average calculation
    Added functionality to calculate batting average based on total runs and innings played.

--- 02-Conditionals And Loops/Intermediate Java Programs/Batting_Average.java
+++ 02-Conditionals And Loops/Intermediate Java Programs/Batting_Average.java
@@ -1,1 +1,22 @@
+// Calculate Batting Average
+
+import java.util.Scanner;
+
+public class Batting_Average {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the total runs scored : ");
+        int totalRuns = sc.nextInt();
+
+        System.out.print("Enter the number of innings played : ");
+        int inningsPlayed = sc.nextInt();
+
+        double battingAverage = (double) totalRuns / inningsPlayed; // Type casting to double for accurate average calculation
+
+        System.out.println("Batting Average : " + battingAverage);
+
+        sc.close();
+    }
+}
 
