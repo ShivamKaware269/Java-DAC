5d43c0f0157cb856e1f74f6d4eaff98d6fcd9cde
Shivam Kaware <shivamkaware269@gmail.com>
2026-08-03 21:48:16 +0530

    Add CGPA calculation program in Java

--- 02-Conditionals And Loops/Intermediate Java Programs/Cgpa.java
+++ 02-Conditionals And Loops/Intermediate Java Programs/Cgpa.java
@@ -1,1 +1,28 @@
+// Calculate CGPA Java Program
+
+import java.util.Scanner;
+
+public class Cgpa {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of subjects: ");
+        int numSubjects = sc.nextInt();
+
+        double totalMarks = 0;
+
+        for (int i = 1; i <= numSubjects; i++) {
+            System.out.print("Enter grade obtained in subject " + i + ": ");
+            double marks = sc.nextDouble();
+            totalMarks += marks;
+        }
+
+        double cgpa = totalMarks / numSubjects;
+
+        System.out.println("Your CGPA is: " + cgpa);
+
+        sc.close();
+    }
+    
+}
 
