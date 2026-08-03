bae880ceb5b01142568c532b81154bac91bf9006
Shivam Kaware <shivamkaware269@gmail.com>
2026-08-03 21:52:16 +0530

    Add Compound Interest calculation program
    This program calculates compound interest based on user input for principal, time, and rate of interest.

--- 02-Conditionals And Loops/Intermediate Java Programs/Compound_Intrest.java
+++ 02-Conditionals And Loops/Intermediate Java Programs/Compound_Intrest.java
@@ -1,1 +1,27 @@
+// Compound Interest Java Program
 
+import java.util.Scanner;
+
+public class Compound_Intrest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int principal, time;
+        double rate, amount, CI;
+
+        System.out.println("Enter Principal amount : ");
+        principal = sc.nextInt();
+
+        System.out.println("Enter Time period : ");
+        time = sc.nextInt();
+
+        System.out.println("Enter Rate of Interest : ");
+        rate = sc.nextDouble();
+
+        // Calculate compound interest
+        amount = principal * Math.pow((1 + rate / 100), time);
+        CI = amount - principal;
+        System.out.println("Compound Interest : " + CI);
+
+        sc.close();
+    }
+}
