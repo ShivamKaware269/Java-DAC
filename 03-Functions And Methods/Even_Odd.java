5ebef2df77a35573f75f2ffaf4b973ef800e9be8
Shivam Kaware <shivamkaware269@gmail.com>
2026-08-04 20:23:25 +0530

    Add Even_Odd class to check even or odd numbers

--- 03-Functions And Methods/Even_Odd.java
+++ 03-Functions And Methods/Even_Odd.java
@@ -1,1 +1,29 @@
+import java.util.Scanner;
+
+public class Even_Odd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); 
+      
+        System.out.print("Enter the number : ");
+        int num = sc.nextInt();
+
+        int x = check(num);
+        if (x == 1) {
+            System.out.println("Number is Even");
+        } else { 
+            System.out.println("Number is Odd");
+        }
+        sc.close();
+    }
+   
+    static int check(int a) {
+
+        if (a % 2 == 0) {
+            return 1;
+        } else { 
+            return 0;
+        }
+    }
+    
+}
 
