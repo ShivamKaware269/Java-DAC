0ff0e8acf64fccef1b368cd8cfd508943a8ac59d
Shivam Kaware <shivamkaware269@gmail.com>
2026-08-05 19:58:04 +0530

    Update Sum_Of_Two.java

--- 03-Functions And Methods/Sum_Of_Two.java
+++ 03-Functions And Methods/Sum_Of_Two.java
@@ -1,1 +1,22 @@
+import java.util.Scanner;
+
+public class Sum_Of_Two {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number 1 : ");
+        int a = sc.nextInt();
+        System.out.print("Enter the number 1 : ");
+        int b = sc.nextInt();
+
+        print(a, b);
+
+        sc.close();
+    }
+
+    static void print(int a, int b) {
+        int sum = a + b;
+        System.out.println("The sum of " + a + " and " + b + " is: " + sum);
+    }
+}
 
