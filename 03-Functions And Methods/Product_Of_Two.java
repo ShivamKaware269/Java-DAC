46486c7d569df3fb6b34b3152358d800e990ca36
Shivam Kaware <shivamkaware269@gmail.com>
2026-08-05 19:59:06 +0530

    Implement product calculation of two numbers

--- 03-Functions And Methods/Product_Of_Two.java
+++ 03-Functions And Methods/Product_Of_Two.java
@@ -1,1 +1,23 @@
+import java.util.Scanner;
 
+public class Product_Of_Two {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number 1 : ");
+        int a = sc.nextInt();
+        System.out.print("Enter the number 2 : ");
+        int b = sc.nextInt();
+
+        int x = calc(a, b);
+
+        System.out.println("The product of " + a + " and " + b + " is: " + x);
+
+        sc.close();
+    }
+
+    static int calc(int a, int b) {
+        int product = a * b;
+        return product;
+    }
+}
