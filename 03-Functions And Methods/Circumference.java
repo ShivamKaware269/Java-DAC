31d6182e80a51bfa07702839f7fe379d97a3b0ec
Shivam Kaware <shivamkaware269@gmail.com>
2026-08-06 21:40:32 +0530

    Add Circumference calculation for a circle

--- 03-Functions And Methods/Circumference.java
+++ 03-Functions And Methods/Circumference.java
@@ -1,1 +1,21 @@
+import java.util.Scanner;
 
+public class Circumference {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the radius of the circle : ");
+        double r = sc.nextDouble();
+
+        double x = calc(r);
+
+        System.out.println("The circumference of the circle is : " + x);
+
+        sc.close();
+    }
+
+    static double calc(double r) {
+        double circumference = 2 * Math.PI * r; // Math.PI is a constant that represents the value of π (pi)
+        return circumference;
+    }
+}
