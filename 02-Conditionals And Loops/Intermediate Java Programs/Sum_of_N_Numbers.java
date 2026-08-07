dd4d1b815cc44f2a2544e1d22356de60298461f2
Shivam Kaware <shivamkaware269@gmail.com>
2026-08-07 21:58:38 +0530

    Add program to calculate sum of first N natural numbers

--- 02-Conditionals And Loops/Intermediate Java Programs/Sum_of_N_Numbers.java
+++ 02-Conditionals And Loops/Intermediate Java Programs/Sum_of_N_Numbers.java
@@ -1,1 +1,19 @@
+import java.util.Scanner;
 
+public class Sum_Of_N_Numbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n, sum = 0;
+
+        System.out.println("Enter a positive integer: ");
+        n = sc.nextInt();
+
+        for (int i = 1; i <= n; ++i) {
+            sum += i;
+        }
+
+        System.out.println("Sum of first " + n + " natural numbers is: " + sum);
+
+        sc.close();
+    }
+}
