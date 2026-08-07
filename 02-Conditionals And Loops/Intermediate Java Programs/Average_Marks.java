5049e08ce1ba73719185886fffb351e370c36137
Shivam Kaware <shivamkaware269@gmail.com>
2026-08-07 21:57:16 +0530

    Implement average marks calculation program

--- 02-Conditionals And Loops/Intermediate Java Programs/Average_Marks.java
+++ 02-Conditionals And Loops/Intermediate Java Programs/Average_Marks.java
@@ -1,1 +1,25 @@
+// Calculate average marks of all subjects
+
+import java.util.Scanner;
+
+public class Average_Marks {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number of subjects: ");
+        int numSubjects = sc.nextInt();
+        int sum = 0;
+        double average;
+
+        System.out.println("Enter marks for each subject: ");
+        for (int i = 0; i < numSubjects; i++) {
+            sum += sc.nextInt();
+        }
+
+        average = (double) sum / numSubjects;
+        System.out.println("Average marks: " + average);
+
+        sc.close();
+    }
+}
 
