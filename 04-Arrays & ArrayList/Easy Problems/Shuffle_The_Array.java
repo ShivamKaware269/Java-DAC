57850363bdb9a69fb76df14f0b426c5c2f0c1a9e
Shivam Kaware <shivamkaware269@gmail.com>
2026-08-09 13:33:57 +0530

    Update Shuffle_The_Array.java

--- 04-Arrays & ArrayList/Easy Problems/Shuffle_The_Array.java
+++ 04-Arrays & ArrayList/Easy Problems/Shuffle_The_Array.java
@@ -1,1 +1,22 @@
+import java.util.Arrays;
 
+public class Shuffle_The_Array {
+    public static void main(String[] args) {
+        int[] nums = {2,5,1,3,4,7};
+        int[] ans = new int[nums.length];
+        int mid = nums.length / 2;
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i % 2 == 0) {
+                ans[i] = nums[count];
+                count++;
+            } else {
+                ans[i] = nums[mid];
+                mid++;
+            }
+        }
+
+        System.out.println(Arrays.toString(ans));
+    }
+}
