6a431c0072907f01e51c3484e7c0fb7f1be73ea5
Shivam Kaware <shivamkaware269@gmail.com>
2026-08-10 19:50:05 +0530

    Add solution for Kid With Greatest Number Of Candies
    Implement logic to determine which kids can have the greatest number of candies after receiving extra candies.

--- 04-Arrays & ArrayList/Easy Problems/Kid_With_Greatest_Number_Of_Candies.java
+++ 04-Arrays & ArrayList/Easy Problems/Kid_With_Greatest_Number_Of_Candies.java
@@ -1,1 +1,35 @@
+// Leetcode - 1431
 
+import java.util.ArrayList;
+
+public class Kid_With_Greatest_Number_Of_Candies {
+    public static void main(String[] args) {
+        int[] candies = {2,3,5,1,3};
+        int extraCandies = 3;
+        int sum = 0, max = 0;
+
+        ArrayList<Boolean> answer = new ArrayList<>();
+
+        for (int i = 0; i < candies.length; i++) {
+            if (max < candies[i]) {
+                max = candies[i];
+            }
+        }
+        System.out.println(max);
+ 
+        for (int i = 0; i < candies.length; i++) {
+            sum = 0;
+            sum = candies[i] + extraCandies;
+
+            if (sum >= max) {
+                answer.add(true);
+            } else {
+                answer.add(false);
+            }
+
+        }
+
+        System.out.println(answer);
+
+    }
+}
