c9d75298ec11ff07a5c2d146b8d7d868f6d632bd
Shivam Kaware <shivamkaware269@gmail.com>
2026-08-10 19:52:22 +0530

    Implement array input and permutation logic
    Added user input functionality to build an array from a permutation.

--- 04-Arrays & ArrayList/Easy Problems/Bulid_Array_From_Permutation.java
+++ 04-Arrays & ArrayList/Easy Problems/Bulid_Array_From_Permutation.java
@@ -1,12 +1,17 @@
+// Leetcode - 1920
+
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Build_Array_From_Permutation {
     public static void main(String[] args) {
+        
         Scanner sc = new Scanner(System.in);
 
         int[] arr = new int[6];
+        
         System.out.println("Enter array elements : ");
+        
         for (int i = 0; i < arr.length; i++) {
             arr[i] = sc.nextInt();
         }
@@ -14,6 +19,7 @@ public class Build_Array_From_Permutation {
         System.out.println("Initial : " + Arrays.toString(arr));
 
         int[] ans = new int[6];
+        
         for (int i = 0; i < arr.length; i++) {
             ans[i] = arr[arr[i]];
         }
