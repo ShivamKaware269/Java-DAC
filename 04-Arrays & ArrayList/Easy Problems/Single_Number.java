4027af16c3d01996aa8e87e72d4322ed3186fd64
Shivam Kaware <shivamkaware269@gmail.com>
2026-08-11 19:49:14 +0530

    Update Single_Number.java

--- 04-Arrays & ArrayList/Easy Problems/Single_Number.java
+++ 04-Arrays & ArrayList/Easy Problems/Single_Number.java
@@ -1,1 +1,15 @@
+public class Single_Number {
+    public static void main(String[] args) {
+        int[] arr = {4, 1, 4, 2, 2};
+        int result = 0;
 
+        // Using XOR Operator
+
+        for (int i : arr) {
+            result ^= i;
+        }
+
+        System.out.println("The unique element in the array is : " + result);
+
+    }
+}
