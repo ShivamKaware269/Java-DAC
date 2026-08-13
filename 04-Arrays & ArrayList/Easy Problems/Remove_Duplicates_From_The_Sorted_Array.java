3285e81365fb96f70c8d916268233c9c74148597
Shivam Kaware <shivamkaware269@gmail.com>
2026-08-13 22:52:31 +0530

    Add duplicate removal logic for sorted array
    Implement a method to remove duplicates from a sorted array and print unique elements.

--- 04-Arrays & ArrayList/Easy Problems/Remove_Duplicates_From_The_Sorted_Array.java
+++ 04-Arrays & ArrayList/Easy Problems/Remove_Duplicates_From_The_Sorted_Array.java
@@ -1,1 +1,20 @@
+import java.util.Arrays;
 
+public class Remove_Duplicates_From_The_Sorted_Array {
+    public static void main(String[] args) {
+        int[] arr = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+        int i = 0, j = 0;
+
+        while (j < arr.length) {
+            if (arr[j] != arr[i]) {
+                i++;
+                arr[i] = arr[j];
+            }
+            j++;
+        }
+
+        System.out.println("Initial " + (i+1) + " elements are unique elements present in the array :");
+        System.out.println(Arrays.toString(arr));
+
+    }
+}
