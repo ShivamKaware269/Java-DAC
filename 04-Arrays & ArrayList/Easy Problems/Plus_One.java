1f366e7248c81251b743da0c9702ee7b4e94791c
Shivam Kaware <shivamkaware269@gmail.com>
2026-08-14 21:46:02 +0530

    Update Plus_One.java

--- 04-Arrays & ArrayList/Easy Problems/Plus_One.java
+++ 04-Arrays & ArrayList/Easy Problems/Plus_One.java
@@ -1,1 +1,26 @@
+import java.util.Arrays;
 
+public class Plus_One {
+    public static void main(String[] args) {
+        int[] array = {1, 5, 7};
+
+        System.out.println(array[array.length - 1]);
+
+        if (array[array.length - 1] != 9) {
+            array[array.length - 1] += 1;
+        }
+
+        for (int i = array.length - 1; i > 0; i--) {
+            
+            if (array[i] == 9) {
+                array[i] = 0;
+                array[i - 1] += 1;
+            }
+        }
+
+        System.out.println(array[array.length - 1]);
+
+        System.out.println(Arrays.toString(array));
+
+    }
+}
