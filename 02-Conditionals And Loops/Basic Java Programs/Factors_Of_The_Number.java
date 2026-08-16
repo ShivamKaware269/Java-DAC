f7bee1038ad806ca66809597538a8c1441227b2d
Shivam Kaware <shivamkaware269@gmail.com>
2026-08-16 18:15:02 +0530

    Update Factors_Of_The_Number.java

--- 02-Conditionals And Loops/Basic Java Programs/Factors_Of_The_Number.java
+++ 02-Conditionals And Loops/Basic Java Programs/Factors_Of_The_Number.java
@@ -1,4 +1,4 @@
-// Input a number and print all the factors of that number (use loops).
+// Take the input as a number and print all the factors existing of that number.
 
 import java.util.Scanner;
 
