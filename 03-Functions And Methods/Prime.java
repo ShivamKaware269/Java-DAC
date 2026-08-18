b52717909d5c6b32ce482b9435941ee01ad7392c
Shivam Kaware <shivamkaware269@gmail.com>
2026-08-18 21:36:48 +0530

    Add prime number checking functionality

--- 03-Functions And Methods/Prime.java
+++ 03-Functions And Methods/Prime.java
@@ -1,1 +1,30 @@
+import java.util.Scanner;
 
+public class Prime {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int number = sc.nextInt();
+
+        int x = checkPrime(number);
+        if (x == 1) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+
+        sc.close();
+    }
+
+    static int checkPrime(int n) {
+        if (n <= 1) {
+            return 0;
+        }
+        for (int i = 2; i <= Math.sqrt(n); i++) { // sqrt(n) is used for optimization, to decrease the number of iterations
+            if (n % i == 0) {
+                return 0;
+            }
+        }
+        return 1;
+    }
+}
