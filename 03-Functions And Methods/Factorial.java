a229a2561def8ef63a498ac4f75a54bdf2ab975d
Shivam Kaware <shivamkaware269@gmail.com>
2026-08-20 21:23:02 +0530

    Implement factorial calculation in Factorial.java

--- 03-Functions And Methods/Factorial.java
+++ 03-Functions And Methods/Factorial.java
@@ -1,1 +1,33 @@
+// 4! = 1 * 2 * 3 * 4 = 24 
+// 3! = 3 * 2 * 1 = 6 
+// 2! = 2 * 1 = 2 
+// Also, 
+// 1! = 1 
+// 0! = 1
 
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int number = sc.nextInt();
+
+        int result = calculateFactorial(number);
+        System.out.println("Factorial of " + number + " is: " + result);
+
+        sc.close();
+    }
+
+    static int calculateFactorial(int n) {
+        if (n < 0) {
+            System.out.println("Factorial is not defined for negative numbers.");
+            return -1; // Return -1 to indicate an error
+        }
+        int factorial = 1;
+        for (int i = 1; i <= n; i++) {
+            factorial *= i;
+        }
+        return factorial;
+    }
+}
