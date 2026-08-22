ccc64ebcc29433c1d9db12d1a9027d2b98772800
Shivam Kaware <shivamkaware269@gmail.com>
2026-08-22 23:04:12 +0530

    Add program to calculate sum of digits of a number

--- 02-Conditionals And Loops/Intermediate Java Programs/Sum_Of_Digits_Of_Number.java
+++ 02-Conditionals And Loops/Intermediate Java Programs/Sum_Of_Digits_Of_Number.java
@@ -1,1 +1,20 @@
+import java.util.Scanner;
 
+public class Sum_Of_Digits_Of_Number {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number : ");
+        int num = sc.nextInt();
+        int DigiSum = 0, rem = 0;
+
+        while (num != 0) {
+            rem = num % 10;
+            DigiSum += rem;
+            num /= 10;
+        }
+
+        System.out.println("The sum of the digits is : " + DigiSum);
+
+        sc.close();
+    }
+}
