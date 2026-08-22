15105b65669ee841eda683853193e211f4925d70
Shivam Kaware <shivamkaware269@gmail.com>
2026-08-22 23:03:20 +0530

    Implement day count for outings in August
    Count the number of even days in August for outings.

--- 02-Conditionals And Loops/Intermediate Java Programs/Month_Out_Count.java
+++ 02-Conditionals And Loops/Intermediate Java Programs/Month_Out_Count.java
@@ -1,1 +1,17 @@
+// Utksrsh is allowed to go out with his friends only on the even days of the given month. Write a program to count the number of days he can go out in the month of August 
 
+public class Month_Out_Count {
+    public static void main(String[] args) {
+        int August = 31;
+        int DaysOut = 0;
+
+        for (int i = 1; i <= August; i++) {
+            if (i % 2 == 0) {
+                DaysOut++;
+            }
+        }
+
+        System.out.println("In the month of August Utkarsh can go out with his friends " + DaysOut + " times");
+
+    }
+}
